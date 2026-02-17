eeb76230b4bf5cd52fbac366710c9455a5467832
Nirmal Kumar Marimuthu <nirmalkumar.marimuthu0@gmail.com>
2026-02-16 18:37:29 -0800

    done with part 1

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -7,13 +7,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Quiz {
+    let title: String
+    let description: String
+    let iconName: String
+}
+
+class ViewController: UITableViewController {
+
+    let quizzes = [
+        Quiz(title: "Mathematics",
+             description: "Test your math skills.",
+             iconName: "math"),
+        Quiz(title: "Marvel Super Heroes",
+             description: "How well do you know Marvel?",
+             iconName: "marvel"),
+        Quiz(title: "Science",
+             description: "Explore the world of science.",
+             iconName: "science")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        title = "iQuiz"
+
+        navigationItem.rightBarButtonItem =
+            UIBarButtonItem(title: "Settings",
+                            style: .plain,
+                            target: self,
+                            action: #selector(showSettings))
     }
 
+    @objc func showSettings() {
+        let alert = UIAlertController(
+            title: "Settings",
+            message: "Settings go here",
+            preferredStyle: .alert
+        )
 
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return quizzes.count
+    }
+
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath)
+        -> UITableViewCell {
+
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "QuizCell")
+        let quiz = quizzes[indexPath.row]
+
+        cell.textLabel?.text = quiz.title
+        cell.detailTextLabel?.text = quiz.description
+        cell.imageView?.image = UIImage(named: quiz.iconName)
+
+        return cell
+    }
 }
-
